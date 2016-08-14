c2c88e79f1489f6ebc6304158748610fc3772701
MirekSz <m.szajowski@gmail.com>
2016-08-14 14:22:15 +0200

    base

--- spring-core-solved/src/main/java/pl/vavatech/auction/blc/model/BaseEntity.java
+++ spring-core-solved/src/main/java/pl/vavatech/auction/blc/model/BaseEntity.java
@@ -1,6 +1,5 @@
 package pl.vavatech.auction.blc.model;
 
-
 public class BaseEntity {
 	private Long id;
 
@@ -10,5 +9,25 @@ public class BaseEntity {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		BaseEntity that = (BaseEntity) o;
+
+		if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return (getId() != null ? getId().hashCode() : 0);
 	}
 }
